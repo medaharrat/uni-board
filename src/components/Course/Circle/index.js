bb77de1524266910c4a032ee2ade8737aa41b2e7
Mohamed Aharrat <mohamedaharrat1@gmail.com>
2021-11-05 13:13:25 +0200

    UB-40/Feat: Implement Discussions

--- src/components/Course/Circle/index.js
+++ src/components/Course/Circle/index.js
@@ -3,7 +3,7 @@ import { useStyles } from './styles';
 import { Box, Divider, Grid } from "@material-ui/core";
 import Intro from "../Intro";
 import Comments from "../../Comments";
-import Content from "../../Content";
+import Discussions from "../../Discussion";
 
 const Circle = ({ course }) => {
     const classes = useStyles();
@@ -11,17 +11,6 @@ const Circle = ({ course }) => {
     return (
         <Box components="div" className={classes.box}>
             <Grid container>
-                {/*
-                <Grid item xs={course.content ? '9': '12'}>
-                    <Intro course={course} />
-                    { Action buttons }
-                </Grid>
-                { course.content && (course.content.length !== 0) && (
-                <Grid item xs={3}>
-                    <Content />
-                </Grid>
-                )}
-                */}
                 <Grid item xs={12}>
                     <Intro course={course} />
                     { /*Action buttons*/ }
@@ -29,8 +18,8 @@ const Circle = ({ course }) => {
             </Grid>
 
             <Divider light className={classes.divider}/>
-
-            <Comments comments={course.comments} /> 
+            { /*Discussions*/ }
+            <Discussions discussions={course.discussions}/>
         </Box>
     );
 }
---
+++ src/components/Discussion/styles.js
@@ -0,0 +1,8 @@
+import { makeStyles } from '@material-ui/core/styles';
+import palette from '../../static/palette';
+
+const useStyles = makeStyles({
+    
+});
+
+export { useStyles };
