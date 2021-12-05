bb009616bed9afef7c175b1f4780c507a2feeaa6
Mohamed Aharrat <mohamedaharrat1@gmail.com>
2021-12-05 00:47:58 +0000

    Prepare to setup router guard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import '../../styles/globals.css'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+      <>
+          <Head>
+              <title>ELTE - Uniboard</title>
+          </Head>
+
+          <div className="app-container bg-light">
+              <div className="container pt-4 pb-4">
+                <Component {...pageProps} />
+              </div>
+          </div>
+      </>
+  );
 }
-
-export default MyApp
+export default App
